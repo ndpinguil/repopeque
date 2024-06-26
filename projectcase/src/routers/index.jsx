7fb7d994788f86a4a264f909bb60000b8ce1741c
ndpinguil <116099207+ndpinguil@users.noreply.github.com>
2024-06-26 17:23:49 -0500

    commit

--- projectcase/src/routers/index.jsx
+++ projectcase/src/routers/index.jsx
@@ -1,13 +1,14 @@
-import {Route , Routes} from "react-router-dom"
-import Auth from "../components/Auth"
-import Welcome from "../components/Welcome"
+import { Route, Routes } from "react-router-dom";
+import Auth from "../components/Auth";
+import Welcome from "../components/Welcome";
 
 const AppRouter = () => {
-    return
-    <Routes>
-        <Route path= "/" elemente={<Welcome/>}/>
-        <Route path= "login" elemente={<Auth/>}/>
-    </Routes>
+    return (
+        <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/login" element={<Auth />} />
+        </Routes>
+    );
 }
 
-export default AppRouter
+export default AppRouter;
